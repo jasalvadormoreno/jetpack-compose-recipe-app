ddbff7842b61c5580437221ca5667d89675764d9
Jose Ángel Salvador Moreno <jasalvador@zimaltec.es>
2021-03-06 17:42:42 +0100

    1.5 Fragments with Jetpack Compose (Interoperability)

--- app/src/main/java/es/jasalvador/recipeapp/MainActivity.kt
+++ app/src/main/java/es/jasalvador/recipeapp/MainActivity.kt
@@ -21,55 +21,10 @@ import androidx.compose.ui.unit.sp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LazyColumn(
-                modifier =
-                Modifier
-                    .fillMaxSize()
-                    .background(color = Color(0xFFF2F2F2))
-            ) {
-                item {
-                    Image(
-                        painter = painterResource(id = R.drawable.happy_meal_small),
-                        contentDescription = "",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(300.dp),
-                        contentScale = ContentScale.Crop,
-                    )
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        ) {
-                            Text(
-                                text = "Happy Meal",
-                                style = TextStyle(fontSize = 26.sp),
-                            )
-                            Text(
-                                text = "$5.99",
-                                style = TextStyle(
-                                    color = Color(0xFF85bb65),
-                                    fontSize = 17.sp,
-                                ),
-                                modifier = Modifier.align(alignment = Alignment.CenterVertically),
-                            )
-                        }
-                        Spacer(modifier = Modifier.padding(top = 10.dp))
-                        Text(
-                            text = "800 Calories",
-                            style = TextStyle(fontSize = 17.sp),
-                        )
-                        Spacer(modifier = Modifier.padding(top = 10.dp))
-                        Button(
-                            onClick = { },
-                            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
-                        ) {
-                            Text(text = "ORDER NOW")
-                        }
-                    }
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
     }
 }
