59067812cfa9d34ae60509667c586d1443f12242
Jose Ángel Salvador Moreno <jasalvador@zimaltec.es>
2021-03-07 11:09:58 +0100

    3.3 Asynchronously Load Images with Glide

--- app/src/main/java/es/jasalvador/recipeapp/presentation/components/RecipeCard.kt
+++ app/src/main/java/es/jasalvador/recipeapp/presentation/components/RecipeCard.kt
@@ -9,11 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import es.jasalvador.recipeapp.R
 import es.jasalvador.recipeapp.domain.model.Recipe
+import es.jasalvador.recipeapp.util.loadPicture
+
+const val DEFAULT_RECIPE_IMAGE = R.drawable.empty_plate
 
 @Composable
 fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
@@ -27,14 +30,17 @@ fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
     ) {
         Column {
             recipe.featuredImage?.let { url ->
-                Image(
-                    painter = painterResource(id = R.drawable.empty_plate),
-                    contentDescription = "",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(225.dp),
-                    contentScale = ContentScale.Crop,
-                )
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE)
+                image.value?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(225.dp),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
             }
             recipe.title?.let { title ->
                 Row(
