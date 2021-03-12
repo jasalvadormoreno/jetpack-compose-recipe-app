3b2677d19f7cc6c829e237dc5a68fdc7e2e406c8
Jose Ángel Salvador Moreno <jasalvador@zimaltec.es>
2021-03-12 21:59:44 +0100

    3.3 Providing the Room Database with Hilt

---
+++ app/src/main/java/es/jasalvador/recipeapp/di/CacheModule.kt
@@ -0,0 +1,30 @@
+package es.jasalvador.recipeapp.di
+
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import es.jasalvador.recipeapp.BaseApp
+import es.jasalvador.recipeapp.cache.RecipeDao
+import es.jasalvador.recipeapp.cache.database.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(app: BaseApp): AppDatabase {
+        return Room.databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeDao(db: AppDatabase): RecipeDao {
+        return db.recipeDao()
+    }
+}
