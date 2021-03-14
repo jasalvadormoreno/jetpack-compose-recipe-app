e21e3accd83b902dba6854231f5b39a1323c17f8
Jose Ángel Salvador Moreno <jasalvador@zimaltec.es>
2021-03-14 17:29:25 +0100

    9.4 Testing SearchRecipes Use-case

--- app/src/test/java/es/jasalvador/recipeapp/interactors/recipe_list/SearchRecipesTests.kt
+++ app/src/test/java/es/jasalvador/recipeapp/interactors/recipe_list/SearchRecipesTests.kt
@@ -5,9 +5,12 @@ import es.jasalvador.recipeapp.cache.AppDatabaseFake
 import es.jasalvador.recipeapp.cache.RecipeDao
 import es.jasalvador.recipeapp.cache.RecipeDaoFake
 import es.jasalvador.recipeapp.cache.model.RecipeEntityMapper
+import es.jasalvador.recipeapp.domain.model.Recipe
 import es.jasalvador.recipeapp.network.RecipeService
 import es.jasalvador.recipeapp.network.data.MockWebServerResponses.recipeListResponse
 import es.jasalvador.recipeapp.network.model.RecipeDtoMapper
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
 import okhttp3.HttpUrl
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -24,6 +27,9 @@ class SearchRecipesTests {
     private lateinit var mockWebServer: MockWebServer
     private lateinit var baseUrl: HttpUrl
     private val appDatabase = AppDatabaseFake()
+
+    private val DUMMY_TOKEN = "fake-token"
+    private val DUMMY_QUERY = "fake-query"
 
     private lateinit var searchRecipes: SearchRecipes
 
@@ -49,12 +55,45 @@ class SearchRecipesTests {
     }
 
     @Test
-    fun mockWebServerSetup() {
+    fun getRecipesFromNetwork_emitRecipesFromCache(): Unit = runBlocking {
         mockWebServer.enqueue(
             MockResponse()
                 .setResponseCode(HttpURLConnection.HTTP_OK)
                 .setBody(recipeListResponse)
         )
+
+        assert(recipeDao.getAllRecipes(1, 30).isEmpty())
+
+        val flowItems = searchRecipes.execute(DUMMY_TOKEN, 1, DUMMY_QUERY, true).toList()
+
+        assert(recipeDao.getAllRecipes(1, 30).isNotEmpty())
+
+        assert(flowItems[0].loading)
+
+        val recipes = flowItems[1].data
+        assert(recipes?.size ?: 0 > 0)
+
+        assert(recipes?.get(index = 0) is Recipe)
+
+        assert(!flowItems[1].loading)
+    }
+
+    @Test
+    fun getRecipesFromNetwork_emitHttpError(): Unit = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+                .setBody("{}")
+        )
+
+        val flowItems = searchRecipes.execute(DUMMY_TOKEN, 1, DUMMY_QUERY, true).toList()
+
+        assert(flowItems[0].loading)
+
+        val error = flowItems[1].error
+        assert(error != null)
+
+        assert(!flowItems[1].loading)
     }
 
     @AfterEach
