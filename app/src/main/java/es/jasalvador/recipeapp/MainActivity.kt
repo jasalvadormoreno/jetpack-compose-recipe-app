ee5f3827732e3fe72f79198b3157dc196920effc
Jose Ángel Salvador Moreno <jasalvador@zimaltec.es>
2021-03-14 15:30:31 +0100

    7.1 Monitoring a Network Connection with ConnectivityManager

--- app/src/main/java/es/jasalvador/recipeapp/MainActivity.kt
+++ app/src/main/java/es/jasalvador/recipeapp/MainActivity.kt
@@ -1,10 +1,16 @@
 package es.jasalvador.recipeapp
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.getSystemService
 import androidx.hilt.navigation.HiltViewModelFactory
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
@@ -15,10 +21,29 @@ import es.jasalvador.recipeapp.presentation.ui.recipe.RecipeDetailScreen
 import es.jasalvador.recipeapp.presentation.ui.recipe.RecipeDetailViewModel
 import es.jasalvador.recipeapp.presentation.ui.recipe_list.RecipeListScreen
 import es.jasalvador.recipeapp.presentation.ui.recipe_list.RecipeListViewModel
+import es.jasalvador.recipeapp.util.TAG
 
 @ExperimentalComposeUiApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cm: ConnectivityManager
+    val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+    val networkCallback = object : ConnectivityManager.NetworkCallback() {
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            Log.d(TAG, "onAvailable: $network")
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Log.d(TAG, "onLost: $network")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -52,5 +77,17 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        cm = getSystemService()!!
+        cm.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cm.unregisterNetworkCallback(networkCallback)
     }
 }
